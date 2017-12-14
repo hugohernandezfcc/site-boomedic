3634339cf6868168c03f04a636c5ff210807b889
unknown <jaziel.moran@doitcloud.consulting>
2017-12-13 18:09:19 -0600

    logo

--- resources/views/vendor/adminlte/register.blade.php
+++ resources/views/vendor/adminlte/register.blade.php
@@ -7,7 +7,11 @@
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/auth.css') }}">
     @yield('css')
 @stop
-
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top" style="color: white;font-weight:200;letter-spacing:1px;font-size: 20px;">Boomedic</a>
+    </div>
+</nav>
 
 @section('body')
     <div class="register-box">
