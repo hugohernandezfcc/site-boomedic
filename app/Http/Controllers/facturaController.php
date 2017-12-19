6795ad311928f82f702b72e784d774b6570c4842
unknown <jaziel.moran@doitcloud.consulting>
2017-12-19 17:23:33 -0600

    ppp3

--- app/Http/Controllers/facturaController.php
+++ app/Http/Controllers/facturaController.php
@@ -13,6 +13,7 @@ class facturaController extends Controller
 	public function fac(){
 	    $payment = DB::table('paymentsmethods')->where('owner','1')->get();
 	    $transaction = DB::table('transaction_bank')->where('paymentmethod',$payment[0]->id)->get();
-	    return view('factura',$transaction);
+	    //return view('factura',$transaction);
+	    return view('factura', compact('codigo'))->with('transaction', $transaction);
 	}
 }
