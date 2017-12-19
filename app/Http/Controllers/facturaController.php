96285d76555a3ce6207ce437032b3ba02f2064ab
unknown <jaziel.moran@doitcloud.consulting>
2017-12-19 17:57:18 -0600

    class

--- app/Http/Controllers/facturaController.php
+++ app/Http/Controllers/facturaController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\Quotation;
+use Auth;
 
 use App\Http\Controllers\Controller;
 
 class facturaController extends Controller
 {
 	public function fac(){
-		
+
 	    $payment = DB::table('paymentsmethods')->where('owner',Auth::id())->get();
 	    $transaction = DB::table('transaction_bank')->where('paymentmethod',$payment[0]->id)->get();
 	    //return view('factura',$transaction);
