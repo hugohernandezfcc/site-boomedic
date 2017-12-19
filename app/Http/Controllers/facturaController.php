2c80f1e6457543db77124c0f9be0ebafcb6577da
unknown <jaziel.moran@doitcloud.consulting>
2017-12-19 17:09:22 -0600

    correccion

--- app/Http/Controllers/facturaController.php
+++ app/Http/Controllers/facturaController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class facturaController extends Controller
 {
-	public function fac()
+	public function fac(){
 	    $payment = DB::table('paymentsmethods')->where('owner','1')->get();
 	    $transaction = DB::table('transaction_bank')->where('paymentmethod',$payment[0]->id)->get();
 	    return view('factura',$transaction);
