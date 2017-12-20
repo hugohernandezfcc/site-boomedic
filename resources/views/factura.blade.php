a918695529a9f5dcc451659650350c0952d0d077
unknown <jaziel.moran@doitcloud.consulting>
2017-12-20 17:26:33 -0600

    correc1

--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -37,7 +37,7 @@
             @foreach ($transaction as $key => $trans)
               <tr>
                   <td>
-                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default" onclick="datosmodal({{$trans->amount}},{{$user->name}},{{$user->email}},{{$user->ocupacion}})">
+                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default" onclick="datosmodal( {{$trans->amount}} , {{$user->name}} , {{$user->email}} , {{$user->occupation}} );">
                       Detalles
                     </button>
                   </td>
