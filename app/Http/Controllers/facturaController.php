91d22d59aa5de3dfd94eeb3a9b3b705dbeba28d1
unknown <jaziel.moran@doitcloud.consulting>
2017-12-20 11:28:14 -0600

    consuln4

--- app/Http/Controllers/facturaController.php
+++ app/Http/Controllers/facturaController.php
@@ -18,14 +18,14 @@ class facturaController extends Controller
 	    	$transaction = DB::table('transaction_bank')->where('paymentmethod',$payment[0]->id)->get();
 	    	if(count($transaction)>0){
 	    		$i=0;
+	    		$transaction2 = new stdClass();
 	    		foreach ($transaction as $key => $t) {
-	    			$transaction2[$i]=[
-	    				'idTransaction_bank'=> $t->id,
-	    				'amount'=>$t->amount,
-	    				'medico'=>'medico'.$i,
-	    			];
+	    			$transaction2[$i]->idTransaction_bank= $t->id;
+	    			$transaction2[$i]->amount=$t->amount;
+	    			$transaction2[$i]->medico=>'medico'.$i;
 	    			$i=$i+1;
 	    		}
+	    		$json = json_encode($transaction2);
 	    		//return view('factura', compact('codigo'))->with('transaction2', $transaction2);
 	    		return view('factura', compact('codigo'))->with('transaction2', $transaction2);
 	    	}
