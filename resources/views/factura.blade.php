0964c4bfb42a2acc5bb16bb308f0ae857a26567f
unknown <jaziel.moran@doitcloud.consulting>
2017-12-20 12:31:30 -0600

    estrellas

--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -65,11 +65,34 @@
               </div>
               <div class="modal-body">
                 <center>
-                  <img src="https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&center=43.2686751,-2.9340005&zoom=16&size=300x300&markers=color:black|43.2686751,-2.9340005" style="width: 40%;" />
+                  <img src="https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&center=43.2686751,-2.9340005&zoom=16&size=600x300&markers=color:black|43.2686751,-2.9340005" style="width: 80%;" />
                 </center>
                 <br>
-                <label>Monto : </label>
-                <label id="idlabelMonto"></label>
+                <div class="padre">
+                  <div >
+                    <label>Monto : </label>
+                    <p class="clasificacion">
+                      <label id="idlabelMonto"></label>
+                    </p>
+                    <label id="">Calificación:  </label>
+                    <form>
+                      <p class="clasificacion">
+
+                        <input id="radio1" type="radio" name="estrellas" value="5"><!--
+                        --><label class="labelEstrellas" for="radio1">★</label><!--
+                        --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+                        --><label class="labelEstrellas" for="radio2">★</label><!--
+                        --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+                        --><label class="labelEstrellas" for="radio3">★</label><!--
+                        --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+                        --><label class="labelEstrellas" for="radio4">★</label><!--
+                        --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+                        --><label class="labelEstrellas" for="radio5">★</label>
+
+                      </p>
+                    </form>
+                  </div>
+                </div>
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
@@ -88,6 +111,22 @@
     <link href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
     <style type="text/css">
       .th{border-style: none;}
+      input[type = "radio"]{ display:none;}
+      .labelEstrellas:hover{color:orange;}
+      .labelEstrellas:hover ~ label{color:orange;}
+      input[type = "radio"]:checked ~ .labelEstrellas{color:orange;}
+      .clasificacion{
+        direction: rtl;/* right to left */
+        unicode-bidi: bidi-override;/* bidi de bidireccional */
+      }
+      .padre{
+        border: 1px;
+        display: inline-block;
+        width: auto;
+        margin: auto;
+        padding: 20px, 20px;
+        text-align: justify;
+      }
   </style>
 @stop
 
