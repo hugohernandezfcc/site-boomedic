6e2c5c3db73f23c235677bf9320db17e10735761
unknown <jaziel.moran@doitcloud.consulting>
2017-12-19 18:18:38 -0600

    ifcambios

--- app/Http/Controllers/facturaController.php
+++ app/Http/Controllers/facturaController.php
@@ -12,9 +12,15 @@ use App\Http\Controllers\Controller;
 class facturaController extends Controller
 {
 	public function fac(){
-		
+
 	    $payment = DB::table('paymentsmethods')->where('owner',Auth::id())->get();
-	    $transaction = DB::table('transaction_bank')->where('paymentmethod',$payment[0]->id)->get();
+	    if(count($payment)>0){
+	    	$transaction = DB::table('transaction_bank')->where('paymentmethod',$payment[0]->id)->get();
+	    	if(count($transaction)>0){
+	    		return view('factura', compact('codigo'))->with('transaction', $transaction);
+	    	}
+	    }
+	    $transaction=[];
 	    //return view('factura',$transaction);
 	    return view('factura', compact('codigo'))->with('transaction', $transaction);
 	}
