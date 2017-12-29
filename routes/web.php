86da422669ada4853f68bee402c0598b5133f46a
Antonio Gervacio <luis.gervaciot@gmail.com>
2017-12-28 18:23:45 -0600

    Test 1823

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/antonio', function (){
     * Descripción: Generacion del sello de una factura en forato xml
     *****************************************************************************/
 
-        $cfdi = file_get_contents('./CFDI.xml','/storage/app/public/xml_test/'); 
+        $cfdi = file_get_contents('CFDI.xml','/storage/app/public/xml_test/'); 
         echo "cfdi sin sellar: ".'<br><br>'.$cfdi;
         //Archivos del CSD de prueba proporcionados por el SAT.
         //ver http://developers.facturacionmoderna.com/webroot/CertificadosDemo-FacturacionModerna.zip
@@ -38,7 +38,7 @@ Route::get('/antonio', function (){
 
     function sellarXML($cfdi, $numero_certificado, $archivo_cer, $archivo_pem){
 
-      //ingresa codificado en base64 el archivo .cer dentro del atributo -certificado- del xml de la facturas
+      //ingresa codificado en base64 el archivo .cer dentro del atributo -certificado- del xml de la factura
       $certificado = str_replace(array('\n', '\r' ), '', base64_encode(file_get_contents($archivo_cer)));
       
       //Genera Cadena Original del XML(factura) y el XSLT(sat)******
