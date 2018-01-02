9bc095f978301ae17fdb242b94d3e21fbf6a1ef6
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-02 12:30:57 -0600

    app.php add laravel xslt

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Krowinski\LaravelXSLT\XSLTServiceProvider::class,
+        Krowinski\LaravelXSLT\XSLTServiceProvider::class, 
 
         JeroenNoten\LaravelAdminLte\ServiceProvider::class,
 
