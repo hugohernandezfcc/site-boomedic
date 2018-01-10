9f23661322bdd0dae2395faf61cac997efa70aea
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-10 11:08:54 -0600

    Test 100118 1108

--- routes/web.php
+++ routes/web.php
@@ -16,69 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/antonio', function (){
-  
-    /***************************************************************************
-    * Descripción: Generaracion del sello de una factura en forato xml
-    *****************************************************************************/
-
-        $cfdi = Storage::disk('xml_test')->get('CFDI.xml'); 
-        echo '<pre>' . str_replace('<', '&lt;', $cfdi) . '</pre>';
-        //echo "cfdi sin sellar: ".'<br><br>'.$cfdi;
-        //Archivos del CSD de prueba proporcionados por el SAT.
-        //ver http://developers.facturacionmoderna.com/webroot/CertificadosDemo-FacturacionModerna.zip
-  
-        $numero_certificado = "00001000000305304226";
-        $archivo_cer = Storage::disk('csd')->get('00001000000305304226.cer');
-        $archivo_key = Storage::disk('csd')->get('00001000000305304226.key');
-        $archivo_pem = Storage::disk('csd')->get('archivo.key.pem');
-        $cadena_original = Storage::disk('public')->get('cadenaoriginal_3_3.xslt');
-
-    //Sellar un XML con los CSD de prueba
-    
-      //ingresa codificado en base64 el archivo .cer dentro del atributo -certificado- del xml de la factura
-      //$certificado = str_replace(array('\n', '\r' ), '', base64_encode(file_get_contents($archivo_cer)));  
-      $certificado = str_replace(array('\n', '\r' ), '', base64_encode($archivo_cer));
-      
-      //Genera Cadena Original del XML(factura) y el XSLT(sat)******
-      $xdoc = new DomDocument("1.0","UTF-8");
-      $xdoc->loadXML($cfdi) or die("XML invalido");
-
-      $XSL = new DOMDocument("1.0","UTF-8");
-      $XSL->load($cadena_original);
-      
-      $proc = new XsltProcessor;
-      $proc->importStyleSheet($XSL);
-      $cadena_original = trim($proc->transformToXML($xdoc)); 
-
-      //aplica metodo de digestion SHA-1 y Firma la información resultante
-      $private = openssl_get_privatekey(file_get_contents($archivo_pem)); //openssl_pkey_get_private
-      
-      openssl_sign($cadena_original, $crypttext, $private, OPENSSL_ALGO_SHA1);
-      openssl_free_key($private);
-      //codfifica en base64 la cadena resultante
-      $sello = base64_encode($crypttext); 
-      //Integra y Guarda la información final al esqueleto del xml, dentro de los atributos especificados
-      $c = $xdoc->getElementsByTagNameNS('http://www.sat.gob.mx/cfd/3', 'Comprobante')->item(0); 
-      $c->setAttribute('sello', $sello);
-      $c->setAttribute('certificado', $certificado);
-      $c->setAttribute('noCertificado', $numero_certificado);
-
-      $cfdi = $xdoc;
-
-      echo '<pre>' . str_replace('<', '&lt;', $cfdi) . '</pre>';
-
-
-/*
-      // adds to main xml /App attributte name template with value  = hello
-  \View::addAttribute('name_template ', 'hello');
-  // create child template to /App with value hello and add aaa and zzz atribute to template.
-  \View::addChild('template', 'hello', false)->addAttribute('aaaa', 'zzz');
-  // creates parent example and adds childs foo and bar to it 
-  \View::addArrayToXmlByChild(['foo', 'bar'], 'example', false); 
-  // add to parent App child bar and zzz
-  \View::addArrayToXml(['bar', 'zzz'], false);
-*/
-  return view('welcome');
+  dd('TEST 100118');
     
 });
 //Route::get('/factura', function () {
