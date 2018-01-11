6b8248efe1fddaedc7aa61fcef4868709ff85130
unknown <jaziel.moran@doitcloud.consulting>
2018-01-11 11:59:00 -0600

    v2.11.2

--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -115,7 +115,7 @@
                 </div>
                 
               <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cerrar</button>
                 <button type="button" class="btn btn-secondary">Guardar cambios</button>
               </div>
             </div>
