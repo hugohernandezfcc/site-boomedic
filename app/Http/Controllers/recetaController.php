e9f44f046e8821b552a9240911233b2e9282472c
unknown <jaziel.moran@doitcloud.consulting>
2018-01-15 12:26:58 -0600

    v1.15.10

--- app/Http/Controllers/recetaController.php
+++ app/Http/Controllers/recetaController.php
@@ -55,6 +55,6 @@ class recetaController extends Controller{
 	    $json= json_encode($x);
 	    //$receta->Data_frontend=$json;
 	    //$receta->save();*/
-		return $json;
+		return json_encode($json);
 	}
 }
