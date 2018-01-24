4571aff08566235b77a221f3af975f196a319b65
unknown <jaziel.moran@doitcloud.consulting>
2018-01-24 16:32:39 -0600

    pruebas email3

--- resources/views/emails/factura_email.blade.php
+++ resources/views/emails/factura_email.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
     </head>
     <body style="">
-    	<img src="{{ $message->embed(public_path() . '/images/logoD.png') }}" width="170" height="150" style="background-color: black; align-self: right" />
+    	
         
         <h2>Envio de Factura.</h2>
         
