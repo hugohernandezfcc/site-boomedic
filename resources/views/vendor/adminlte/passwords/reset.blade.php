34fbe51832072e28fd5e16cb194c8867498bf451
unknown <jaziel.moran@doitcloud.consulting>
2018-01-31 16:07:17 -0600

    vista reset

--- resources/views/vendor/adminlte/passwords/reset.blade.php
+++ resources/views/vendor/adminlte/passwords/reset.blade.php
@@ -7,12 +7,10 @@
     @yield('css')
 @stop
 
-@section('body_class', 'login-page')
-
 @section('body')
     <div class="login-box">
         <div class="login-logo">
-            <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</a>
+            <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}" style="font-family:Catamaran,Helvetica,Arial,sans-serif;color: white;">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</a>
         </div>
         <!-- /.login-logo -->
         <div class="login-box-body">
