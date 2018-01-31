c644c5953acbc9a4ef772137397919a4944ca6b7
unknown <jaziel.moran@doitcloud.consulting>
2018-01-31 12:42:08 -0600

    fondos 10

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -179,7 +179,7 @@
       <div class="overlay"></div>
     </section>
 
-    <section class="contact " id="contact" style="background-image: url('img/fondo-04.jpg');background-size: 100% auto; color: white; background-position: bottom;">
+    <section class="contact " id="contact" style="background-image: url('img/fondo-04.jpg');background-size: cover; color: white; background-position: bottom;">
       <div class="container">
         <h2>@lang('main.text_contact2')</h2>
         <ul class="list-inline list-social">
@@ -203,7 +203,7 @@
     </section>
 
     <!-- Receta médica -->
-    <section class="contact " id="receta" style="width: 100%; color: white;-moz-transform: scaleY(-1);-o-transform: scaleY(-1);-webkit-transform: scaleY(-1);transform: scaleY(-1);filter: FlipV;background-image: url('img/fondo-04.jpg');background-size: 100% auto;background-position: bottom;">
+    <section class="contact " id="receta" style="width: 100%; color: white;-moz-transform: scaleY(-1);-o-transform: scaleY(-1);-webkit-transform: scaleY(-1);transform: scaleY(-1);filter: FlipV;background-image: url('img/fondo-04.jpg');background-size: cover;background-position: bottom;">
       <div class="container" style="-moz-transform: scaleY(-1);-o-transform: scaleY(-1);-webkit-transform: scaleY(-1);transform: scaleY(-1);filter: FlipV;">
             <a href="/receta" class="btn btn-outline btn-xl js-scroll-trigger" >@lang('main.prescription')</a>
         </div>
