5a88677483e7b0d65ea3fcb646b612a7351aa878
unknown <jaziel.moran@doitcloud.consulting>
2018-02-07 17:31:51 -0600

    prueba 17

--- app/Http/Controllers/facturaController.php
+++ app/Http/Controllers/facturaController.php
@@ -27,13 +27,13 @@ class facturaController extends Controller
 
     public function qualification(Request $request,$id){
         $qua = medical_appointment::find($id);
-        /*if($qua->qualification == null && $request->qualification != 0){
+        if($qua->qualification == null && $request->qualification != 0){
             $qua->qualification = $request->qualification;
             $qua->save();
             return 'guardado';
         }else{
             return 'ya calificado, o no calificó';
-        }*/
-        return $qua->user_doctor;
+        }
+        //return $qua->user_doctor;
     }
 }
