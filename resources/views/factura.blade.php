7bb055234c47cf878742833a31417297c1906de6
unknown <jaziel.moran@doitcloud.consulting>
2018-02-07 11:39:45 -0600

    prueba2

--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -224,7 +224,7 @@
     };
     function timbrado(){
       var conceptos = [{'claveProdServ' : '01010101', 'cantidad' : 1, 'claveUnidad' : 'H87', 'tipoUnidad' : 'Pieza', 'descripcion' : 'Consulta en el área de '+$("#especialidad").text(), 'valorUnitario' : montoM, 'importe' : montoM}];
-      var dat = {'nombreEmisor' : 'EMISOR PRUEBA SA DE CV', 'rfcEmisor' : 'LAN7008173R5', 'regimenFiscal' : '601', 'subtotal' : montoM, 'total' : montoM, 'lugarExpedicion' : codigoPostalM, 'formaPago' : '03', 'condicionesPago' : 'CONTADO', 'metodoPago' : 'PUE', 'conceptos' : conceptos};
+      var dat = {'nombreEmisor' : 'EMISOR PRUEBA SA DE CV', 'rfcEmisor' : 'LAN7008173R5', 'regimenFiscal' : '601', 'subtotal' : document.getElementById("idlabelMonto").innerHTML, 'total' : montoM, 'lugarExpedicion' : codigoPostalM, 'formaPago' : '03', 'condicionesPago' : 'CONTADO', 'metodoPago' : 'PUE', 'conceptos' : conceptos, 'moneda' : 'MXN'};
 
       document.getElementById("carga2").className = "overlay";
       document.getElementById("carga2").innerHTML = "<i class='fa fa-refresh fa-spin' id='imgCarga'></i>";
