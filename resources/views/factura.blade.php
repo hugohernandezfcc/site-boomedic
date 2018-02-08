411c172e07427e5aa0ec01cda05cb3efcf8d86e4
unknown <jaziel.moran@doitcloud.consulting>
2018-02-08 17:38:01 -0600

    prueba 3

--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -95,7 +95,7 @@
               <label class="label1">Monto : </label>
               <label class="label1">Calificación:  </label>
             </div>
-            <div  class="div2" style=" width: 45%;display: inline-block;text-align: left;">
+            <div  class="" style=" width: 45%;display: inline-block;text-align: left;">
               <label class="label2" id="nombre"></label>
               <label class="label2" id="email"></label>
               <label class="label2" id="especialidad"></label>
